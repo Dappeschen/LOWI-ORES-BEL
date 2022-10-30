9e333c0792432181a3707788f79b0bda83454dbe
Dappeschen <martin_roell@yahoo.com>
2022-10-30 17:42:55 +0100

    Create Mosquitto_subscriber_LOWI-42.py
    Adding my first pyhton file on LOWI project - basic connect to LOWI data via free eclipse mosqitto MQTT server / broker - test. mosquitto.org

---
+++ Mosquitto_subscriber_LOWI-42.py
@@ -0,0 +1,29 @@
+'''
+Created on 24 oct. 2022
+
+@author: MRO
+'''
+
+import paho.mqtt.client as mqtt
+import time
+
+def on_message(client, userdata, message):
+    print("received message: " ,str(message.payload.decode("utf-8")))
+
+mqttBroker ="test.mosquitto.org" 
+
+client = mqtt.Client("LOWI-42")
+client.connect(mqttBroker) 
+
+client.loop_start()
+
+client.subscribe("3494546c709b/PUB/CH0")
+client.on_message=on_message 
+
+time.sleep(300000) #wait 
+
+print("done")
+
+client.loop_stop()
+
+
