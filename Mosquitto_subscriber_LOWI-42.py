d37191b44a5ab6b378ceb28e78fe9390ce512073
Dappeschen <dappeschen@artware-solutions.org>
2022-11-01 19:29:32 +0100

    Update Mosquitto_subscriber_LOWI-42.py
    Added time stamp

--- Mosquitto_subscriber_LOWI-42.py
+++ Mosquitto_subscriber_LOWI-42.py
@@ -10,13 +10,26 @@ import time
 import datetime
 from datetime import datetime
 import pytz                         # for timezone()
-
+import json
+ 
 
 def on_message(client, userdata, message):
     #Date and Tie in Central Europr / Brussels, Belgium
     dt_eur = datetime.now(pytz.timezone('Europe/Brussels'))
-    #print MQTT received message's payload, predceeded by date/time stamp CET
-    print(dt_eur.strftime("%Y:%m:%d %H:%M:%S %Z")," ", str(message.payload.decode("utf-8")))
+    
+    #prepare to print MQTT received message's payload, preceeded by date/time stamp - in Central Europe Time (CET) time zone 
+    json_string = str(message.payload.decode("utf-8"))
+    
+    #claculate length / number of characters of message's payload
+    payload_length = len(json_string)
+    
+    #convert payload info in string "json_string" in json format into Python string format
+    y = json.loads(json_string) 
+    
+    print("LOWI's MAC Address: ", y["ident"])
+    
+    #print MQTT received message's payload, preceeded by date/time stamp - in Central Europe Time (CET) time zone 
+    print(dt_eur.strftime("%Y:%m:%d %H:%M:%S %Z")," ", json_string)
     
 #Preparing to connect to free MQTT test / broker server MOSQUITTO.org
 mqttBroker ="test.mosquitto.org" 
@@ -27,6 +40,7 @@ client.connect(mqttBroker)
 client.loop_start()
 
 client.subscribe("3494546c709b/PUB/CH0")
+
 client.on_message=on_message 
 
 time.sleep(300000) #wait 
