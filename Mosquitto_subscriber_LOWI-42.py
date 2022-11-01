ddd641b3c2140a2074da1f10d3dfb7ce9c0db9a7
Dappeschen <dappeschen@artware-solutions.org>
2022-11-01 17:37:13 +0100

    Update Mosquitto_subscriber_LOWI-42.py
    Added concrete installation IDs and date/time stamp

--- Mosquitto_subscriber_LOWI-42.py
+++ Mosquitto_subscriber_LOWI-42.py
@@ -1,16 +1,24 @@
 '''
-Created on 24 oct. 2022
-
-@author: Dappeschen
-#tested with PyDev Eclipse IDE 2209
+Created on        24 October 2022
+Last updated on   1 November 2022
+@author:          Dappeschen
+Installation      Data ORES elctrical energy meter with Qonnex 2Wire Belgium LOWI3- ref.: 3494546c709b
 '''
 
 import paho.mqtt.client as mqtt
 import time
+import datetime
+from datetime import datetime
+import pytz                         # for timezone()
+
 
 def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
-
+    #Date and Tie in Central Europr / Brussels, Belgium
+    dt_eur = datetime.now(pytz.timezone('Europe/Brussels'))
+    #print MQTT received message's payload, predceeded by date/time stamp CET
+    print(dt_eur.strftime("%Y:%m:%d %H:%M:%S %Z")," ", str(message.payload.decode("utf-8")))
+    
+#Preparing to connect to free MQTT test / broker server MOSQUITTO.org
 mqttBroker ="test.mosquitto.org" 
 
 client = mqtt.Client("LOWI-42")
@@ -18,7 +26,7 @@ client.connect(mqttBroker)
 
 client.loop_start()
 
-client.subscribe("<put your LOWI's 12 digit MAC address here>/PUB/CH0")
+client.subscribe("3494546c709b/PUB/CH0")
 client.on_message=on_message 
 
 time.sleep(300000) #wait 
